9871ce4f6a39b93406f5afecbd545e37917ad4c4
Dan Souza <souza.music@outlook.com>
2022-03-10 11:54:46 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('%c HI', 'color: firebrick')
+const dog = document.querySelector("#breed-dropdown")
+for (let i = 0; i < dog; i++);
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(result => console.log(result))
+    var img = document.createElement('IMG')
+    img.addEventListener('load', function () {
+
+    }, true);
+    img.src = imgUrl
+    
+    
+})
+
+
+
+
+/*  
+
+, {
+    method: 'POST',
+    body:imgUrl,
+})
+    .then(response => response.json())
+    .then(result => {
+        console.log('success', result)
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    })
+})*/
+
