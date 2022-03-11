d758a8ac4c27b1d7bdb9d8313cec2c587d01ff6c
Dan Souza <souza.music@outlook.com>
2022-03-10 16:28:42 -0800

    update

--- src/index.js
+++ src/index.js
@@ -1,55 +1,39 @@
-const pics = document.getElementById("dog-image-container")
 const dog = document.querySelector("#breed-dropdown")
 for (let i = 0; i < dog; i++);
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
 
 document.addEventListener('DOMContentLoaded', (e) => {
+    const pics = document.querySelector("#dog-image-container")
     fetch(imgUrl)
         .then(response => response.json())
-        .then(result => {
-            const p = result.message
-            p.forEach((pic) =>  {
-                console.log(pic)
+        .then(pictures => {
+            const p = pictures.message
+            p.forEach((pic) => {
+                const img = document.createElement('img')
+                img.src = `${pic}`
+                pics.appendChild(img)
+                img.style.width = '400px'
+                img.style.height = '400px'
             })
-
+        });
+    const types = document.querySelector('#dog-breeds')
+    fetch(breedUrl)
+        .then(response => response.json())
+        .then(breeds => {
+            const bl = Object.keys(breeds.message)
+            bl.forEach((breed) => {
+                const li = document.createElement('li')
+                li.innerText = `${breed}`
+                types.appendChild(li)
+            })
         })
+})
+
+function colorText() {
+    document.addEventListener("click", function(){
+    var col = document.getElementById('li').fontcolor("red")
+    
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*async function fetchPics() {
-    const response = await fetch(imgUrl)
-    const result = await response.json();
-
-    result.forEach(imgUrl => {
-   
-            img => {
-           Object.entries(imgUrl).forEach(([key, value]) => {
-          var img = document.createElement('IMG')
-                img.addEventListener('load', function () {
-
-                }, true);
-                img.src = imgUrl
-            }
-        })
-    })
-
 }
-*/
-
-
-
-
-
